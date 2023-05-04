ba6423c535aa5802d86d98767948a0eacc186103
ashooshtari <ashooshtari@benevity.com>
2023-05-04 05:06:50 -0700

    Add Toast component to display a notification with an option to like a form submission. This component will be displayed whenever a new mock form submission is created, allowing the user to like the submission or dismiss the notification.

---
+++ src/Toast.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
+
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
+export default function Toast({ open, onClose, message, action }) {
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    onClose();
+  };
+
+  return (
+    <Snackbar
+      anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+      open={open}
+      autoHideDuration={6000}
+      onClose={handleClose}
+    >
+      <Alert onClose={handleClose} severity="success" action={action}>
+        {message}
+      </Alert>
+    </Snackbar>
+  );
+}
