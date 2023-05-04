b78b8a6dc9fff47c26cddac65d57ae3a23ecab7e
ashooshtari <ashooshtari@benevity.com>
2023-05-04 03:55:51 -0700

    feat: Render list of liked submissions in Content component
    This commit adds functionality to the Content component to render a list of liked form submissions. The likedSubmissions prop is passed down from the App component as an array of submission objects. The Content component uses the map method to iterate over this array and render each submission as a Typography component with the submission text.
    
    Changes made to Content.js:
    
    Added map method to render list of liked submissions
    Updated TODO text to indicate that it is no longer needed

--- src/Content.js
+++ src/Content.js
@@ -2,14 +2,16 @@ import React from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 
-export default function Content() {
+export default function Content({ likedSubmissions }) {
   return (
-    <Box sx={{marginTop: 3}}>
+    <Box sx={{ marginTop: 3 }}>
       <Typography variant="h4">Liked Form Submissions</Typography>
 
-      <Typography variant="body1" sx={{fontStyle: 'italic', marginTop: 1}}>
-        TODO: List of liked submissions here (delete this line)
-      </Typography>
+      {likedSubmissions.map((submission) => (
+        <Typography key={submission.timestamp} variant="body1" sx={{ fontStyle: 'italic', marginTop: 1 }}>
+          {submission.text}
+        </Typography>
+      ))}
     </Box>
   );
 }
