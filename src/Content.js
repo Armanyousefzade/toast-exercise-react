3ef5d3149a1336322a3fc49471bb13f6db8f7b3a
ashooshtari <ashooshtari@benevity.com>
2023-05-04 04:47:30 -0700

    Refactor Content component to use Material-UI styling and display additional submission details.
    This commit refactors the Content component to use Material-UI Box and Typography components for improved styling and consistency with the rest of the application. It also adds additional submission details such as first name, last name, and email address to provide more context for the user.

--- src/Content.js
+++ src/Content.js
@@ -1,14 +1,35 @@
 import React from 'react';
-import { Typography } from '@material-ui/core';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
-function Content(props) {
+export default function Content({likedSubmissions}) {
   return (
-    <div>
-      {props.likedSubmissions.map((submission) => (
-        <Typography key={submission.timestamp}>{submission.text}</Typography>
-      ))}
-    </div>
+    <Box sx={{marginTop: 3}}>
+      <Typography variant="h4">Liked Form Submissions</Typography>
+      {likedSubmissions.length > 0 ? (
+        likedSubmissions.map((submission) => (
+          <Box
+            key={submission.id}
+            sx={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              backgroundColor: '#f0f0f0',
+              borderRadius: 5,
+              padding: 2,
+              marginTop: 1,
+            }}
+          >
+            <Box sx={{ flexGrow: 1 }}>
+              <Typography variant="body1">{`${submission.firstName} ${submission.lastName} - ${submission.email}`}</Typography>
+            </Box>
+          </Box>
+        ))
+      ) : (
+        <Typography variant="body1" sx={{ fontStyle: 'italic', marginTop: 1 }}>
+          No liked submissions
+        </Typography>
+      )}
+    </Box>
   );
 }
-
-export default Content;
