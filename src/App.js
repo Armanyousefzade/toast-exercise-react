769e4a918c42db04bad78b88568bb1d15aeb5d70
ashooshtari <ashooshtari@benevity.com>
2023-05-04 03:45:51 -0700

    feat: Add functionality to fetch and display liked form submissions
    In this commit, we have added the functionality to fetch and display the list of liked form submissions. We have imported the useEffect and useState hooks from React and the getLikedSubmissions function from ./service/mockServer. We have used the useState hook to initialize a state variable likedSubmissions with an empty array. We have then used the useEffect hook to fetch the liked submissions from the mock server by calling getLikedSubmissions and updating the likedSubmissions state variable. We have passed likedSubmissions as a prop to the Content component to display the list of liked form submissions.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Container from '@mui/material/Container';
 
 import Header from './Header';
 import Content from './Content';
+import { getLikedSubmissions } from './service/mockServer';
 
 function App() {
+  const [likedSubmissions, setLikedSubmissions] = useState([]);
+
+  useEffect(() => {
+    async function fetchLikedSubmissions() {
+      const submissions = await getLikedSubmissions();
+      setLikedSubmissions(submissions);
+    }
+    fetchLikedSubmissions();
+  }, []);
+
   return (
     <>
       <Header />
       <Container>
-        <Content />
+        <Content likedSubmissions={likedSubmissions} />
       </Container>
     </>
   );
